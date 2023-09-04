8664a9419a0c76507547b280e3d6949fee997edb
CaueCostacurta <cauecostacurta@outlook.com>
2023-09-04 11:31:50 -0300

    Created Scaffold Student pages with CRUD, updated the database context class and added the database exception filter installing the  Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore in nuget

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ContosoUniversity.Data;
 namespace ContosoUniversity
 {
     public class Program
@@ -5,9 +8,13 @@ namespace ContosoUniversity
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<SchoolContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolContext") ?? throw new InvalidOperationException("Connection string 'SchoolContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddRazorPages();
+
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             var app = builder.Build();
 
@@ -17,6 +24,11 @@ namespace ContosoUniversity
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
             }
 
             app.UseHttpsRedirection();
